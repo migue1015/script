1a9e0971497fe291aa8e7fcf029de94d3bfe79d7
migue1015 <migue1015@users.noreply.github.com>
2018-04-10 23:20:27 -0500

    script

---
+++ dias.js
@@ -0,0 +1,26 @@
+intmain(Diassemana)
+{
+int(i);
+printf("Escriba un numero del 1 al 7: ");
+    newFunction();
+
+switch(i){
+case 1: printf ("Lunes\n");
+break;
+case 2: printf ("Martes\n");
+break;
+case 3: printf ("Miércoles\n");
+break;
+case 4: printf ("Jueves\n");
+break;
+case 5: printf ("Viernes\n");
+break;
+case 6: printf ("Sábado\n");
+break;
+case 7: printf ("Domingo\n");
+break;
+default: printf ("Opción no válida\n");
+break;
+}
+return 0;
+}
---
+++ pares.js
@@ -0,0 +1,7 @@
+newFunction();
+
+function newFunction() {
+    for (let i = 0; 1 <= 25; i++) {
+        console.log(String.fromCharCode(i + 97));
+    }
+}
